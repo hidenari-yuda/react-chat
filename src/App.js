b759cc771f5a444da5e289b1cfdf3e4f0b3e5a09
hidenari-yuda <dehinari7302@icloud.com>
2022-08-06 15:58:25 +0900

    chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SignIn from './components/SignIn';
+import Line from './components/Line';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase.js';
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
-    <div className="App">
-      <SignIn/>
-    </div>
+    <div className="App">{ user ? <Line /> : <SignIn /> }</div>
   );
 }
 
---
+++ src/components/Line.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import SignOut from './SignOut'
+import SendMessage from './SendMessage'
+import { db, auth } from '../firebase.js'
+
+function Line() {
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot((snapshot) => { setMessages(snapshot.docs.map((doc) => doc.data()));
+        });
+        },[]);
+  return (
+    <div>
+        <SignOut />
+    <div className="msgs">
+        {messages.map(({id, text, photoURL, uid}) => (
+            <div>
+                <div key={id} className={`msg ${uid === auth.currentUser.uid ? "sent" : "receitved"}`}>
+                    <img src={photoURL} alt="" />
+                    <p>{text}</p>
+                    </div>
+            </div>
+))}
+    </div>
+    <SendMessage />
+    </div>
+  )
+}
+
+export default Line
